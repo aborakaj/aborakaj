826cda87289116ce2dfec8282b22a7010318eb38
floristafa <fstafa@ritech.co>
2024-02-06 11:48:26 +0100

    restructured folders

---
+++ src/app/components/pages/desks/desks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desks',
+  standalone: true,
+  imports: [],
+  templateUrl: './desks.component.html',
+  styleUrl: './desks.component.scss'
+})
+export class DesksComponent {
+
+}
---
+++ src/app/components/pages/organizations/organizations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizations',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizations.component.html',
+  styleUrl: './organizations.component.scss'
+})
+export class OrganizationsComponent {
+
+}
