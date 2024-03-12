a67e02db4697ff02de2071cb23c26c36ebff7498
floristafa <fstafa@ritech.co>
2024-03-12 19:52:37 +0100

    modal refactor

--- src/app/shared/components/custom-modal/modal.component.ts
+++ src/app/shared/components/custom-modal/modal.component.ts
@@ -8,6 +8,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ModalComponent {
   @Input() actionButtonLabel: string = '';
   @Input() isDisplayModal: boolean = false;
+  @Input() isActionButtonDisabled?: boolean = false;
+  @Input() style?: object;
+  @Input() header?: string;
+
+
   @Output() onActionButtonClick = new EventEmitter<void>();
   @Output() close = new EventEmitter<void>();
 
