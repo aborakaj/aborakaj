4a93d4349af711ed434493f59d892342ac123967
floristafa <fstafa@ritech.co>
2024-03-18 15:12:08 +0100

    update

--- src/app/shared/components/custom-modal/modal.component.ts
+++ src/app/shared/components/custom-modal/modal.component.ts
@@ -6,23 +6,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent {
+  private defaultStyles: object = { width: '640px' }
+
   @Input() actionButtonLabel: string = '';
   @Input() isDisplayModal: boolean = false;
   @Input() isActionButtonDisabled?: boolean = false;
-  @Input() style?: any;
   @Input() header?: string;
-
+  @Input() set style(incomingStyles: object) {
+    this.defaultStyles = { ...this.defaultStyles, ...incomingStyles };
+  }
 
   @Output() onActionButtonClick = new EventEmitter<void>();
   @Output() close = new EventEmitter<void>();
 
-  ngOnInit() {
-
-    if (!this.style) {
-      this.style = { width: '640px' };
-    } else if (!this.style.width) {
-      this.style = { ...this.style, width: '640px' };
-    }
+  get style(): object {
+    return this.defaultStyles;
   }
 
   handleActionButtonClick() {
