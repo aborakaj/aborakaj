d1856c20636aa51969ae92c8be9cfd8f8f2345e8
romeollangozi <romeollangozi123@gmail.com>
2024-04-14 01:07:09 +0200

    added return types

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,25 +8,25 @@ import { catchError, throwError } from 'rxjs';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  public getAll<T>(url: string) {
+  public getAll<T>(url: string): Observable<T> {
     return this.http.get<T>(url).pipe(catchError(this.handleErrors));
   }
 
-  public getOne<T>(url: string, id: string) {
+  public getOne<T>(url: string, id: string): Observable<T> {
     return this.http.get<T>(`${url}/${id}`).pipe(catchError(this.handleErrors));
   }
 
-  public add<T>(url: string, payload: T) {
+  public add<T>(url: string, payload: T): Observable<T> {
     return this.http.post<T>(url, payload).pipe(catchError(this.handleErrors));
   }
 
-  public delete<T>(url: string, id: string) {
+  public delete<T>(url: string, id: string): Observable<T> {
     return this.http
       .delete<T>(`${url}/${id}`)
       .pipe(catchError(this.handleErrors));
   }
 
-  public update<T, U = {}>(url: string, id: string, payload: U) {
+  public update<T, U = {}>(url: string, id: string, payload: U): Observable<T> {
     return this.http
       .put<T>(`${url}/${id}`, payload)
       .pipe(catchError(this.handleErrors));
